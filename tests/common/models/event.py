8caad51651e63dc17520c4c24836244945e22d0e
hochan <106953872+cheis11@users.noreply.github.com>
2026-09-03 21:36:21 +0900

    feat(데이터 파이프라인): EventType Enum 구현

--- tests/common/models/event.py
+++ tests/common/models/event.py
@@ -6,3 +6,11 @@ class EventSource(str, Enum):
     POWERSHELL = "powershell"
     SECURITY = "security"
     VELOCIRAPTOR = "velociraptor"
+
+
+class EventType(str, Enum):
+    PROCESS_CREATE = "process_create"
+    NETWORK_CONNECTION = "network_connection"
+    SCRIPT_BLOCK = "script_block"
+    FILE_CREATE = "file_create"
+    REGISTRY_CHANGE = "registry_change"
